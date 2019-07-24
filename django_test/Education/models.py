1aae43eea0b7583d78bbebe2079bc3a63603a3a2
Boqi Chen <boqi.chen@mail.mcgill.ca>
2019-07-23 23:27:45 -0400

    add data and map

--- django_test/Education/models.py
+++ django_test/Education/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta  
 from django.contrib.auth.models import User
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content =models.TextField()
-    date_posted=models.DateTimeField(default=timezone.now)
+    date_posted=models.DateTimeField(default=timezone.now())
     Teacher = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__ (self):
         return self.title
+
+class School(models.Model):
+	name = models.CharField(max_length=100)
+	latitude = models.IntegerField()
+	longitude = models.IntegerField()
+
+class Person(models.Model):
+	name = models.CharField(max_length=100)
+	school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+class Class(models.Model):
+	name = models.CharField(max_length=100)
+	school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+class Student(Person):
+	class_name = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+class Learning(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+class MovableResource(models.Model):
+	name = models.CharField(max_length=100)
+	availableStart = models.DateTimeField(default=timezone.now())
+	availableEnd = models.DateTimeField(default=timezone.now() + timedelta(days=1) )
--- django_test/Education/views.py
+++ django_test/Education/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-
+mapbox_access_token = 'pk.eyJ1IjoicTUzNjc3NjEwIiwiYSI6ImNqeWdvZnk3NjAzOG0zYm8wbWcycXJ6ZmsifQ.qJRHm8_RkqqT5rc-EaFW6g'
 # Create your views here.
 def home(request):
     context = {
@@ -9,4 +9,6 @@ def home(request):
     return render(request,'Education/home.html',context)
 
 def about(request):
-    return render(request,'Education/Map.html',{'title':'Map'})
+    return render(request,'Education/Map.html',{
+    	'title':'Map',
+    	'mapbox_access_token': mapbox_access_token})
